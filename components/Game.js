8ec374b8af1a070bd8d626a5f6b868ef5be7c1f3
Isaias Pomales <impomales@gmail.com>
2018-06-01 17:48:32 -0400

    game logic complete

--- components/Game.js
+++ components/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { StackNavigator } from "react-navigation";
 import { emojis } from "../utils";
 
@@ -18,20 +18,72 @@ class Game extends Component {
     this.state = {
       emojis: [],
       randomQuestion: 0,
-      score: 0
+      guess: "",
+      message: "",
+      score: 0,
+      isActive: true
     };
     this.restartGame = this.restartGame.bind(this);
     this.pickRandomQuestion = this.pickRandomQuestion.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.checkGuess = this.checkGuess.bind(this);
   }
 
   componentDidMount() {
     this.restartGame();
   }
 
+  handleChange(guess) {
+    this.setState({ guess });
+  }
+
+  checkGuess() {
+    let {
+      score,
+      message,
+      randomQuestion,
+      emojis,
+      guess,
+      isActive
+    } = this.state;
+    guess = guess.replace(/\W/g, "").toLowerCase();
+    const answer = emojis[randomQuestion].answer
+      .replace(/\W/g, "")
+      .toLowerCase();
+    if (guess === answer) {
+      score += 10;
+      message = "Nice Job!";
+      emojis = emojis.filter((emoji, index) => index !== randomQuestion);
+      randomQuestion = this.pickRandomQuestion(emojis);
+    } else {
+      message = "Try Again!";
+    }
+
+    if (!emojis.length) {
+      message = "You win!";
+      isActive = false;
+    }
+    this.setState({
+      guess: "",
+      score,
+      message,
+      randomQuestion,
+      emojis,
+      isActive
+    });
+  }
+
   restartGame() {
     const emojisArr = emojis.slice();
     const randomQuestion = this.pickRandomQuestion(emojisArr);
-    this.setState({ emojis: emojisArr, randomQuestion, score: 0 });
+    this.setState({
+      emojis: emojisArr,
+      randomQuestion,
+      score: 0,
+      guess: "",
+      message: "",
+      isActive: true
+    });
   }
 
   pickRandomQuestion(arr) {
@@ -39,12 +91,24 @@ class Game extends Component {
   }
 
   render() {
+    const { emojis, randomQuestion, score, message } = this.state;
     return (
       <View style={styles.container}>
-        {this.state.emojis.map((emoji, index) => (
-          <Text key={index}>{emoji.question}</Text>
-        ))}
-        <Text>SCORE: {this.state.score}</Text>
+        <Text>{message}</Text>
+        <Text>SCORE: {score}</Text>
+        {this.state.isActive && (
+          <View>
+            {emojis.length && <Text>{emojis[randomQuestion].question}</Text>}
+            <TextInput
+              onChangeText={this.handleChange}
+              value={this.state.guess}
+              placeholder="Guess the Phrase!"
+            />
+
+            <Button onPress={this.checkGuess} title="Guess!" />
+          </View>
+        )}
+        <Button onPress={this.restartGame} title="New Game" />
       </View>
     );
   }
