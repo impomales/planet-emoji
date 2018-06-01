c7b1d6d9f2349859c9de6182796541f4b910f20c
Isaias Pomales <impomales@gmail.com>
2018-06-01 16:33:39 -0400

    navigational setup complete

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import Home from "./components/Home";
+import Game from "./components/Game";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <RootNavigator />;
   }
 }
+
+const RootNavigator = createStackNavigator({
+  Main: {
+    screen: Home,
+    navigationOptions: {
+      headerTitle: "Home"
+    }
+  },
+  Game: {
+    screen: Game,
+    navigationOptions: {
+      headerTitle: "Planet Emoji"
+    }
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
