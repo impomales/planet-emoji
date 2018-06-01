d6107755b96f5677486018eef5c32de57a89ca8a
Isaias Pomales <impomales@gmail.com>
2018-06-01 16:56:16 -0400

    game state setup complete

--- components/Game.js
+++ components/Game.js
@@ -1,12 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { StackNavigator } from "react-navigation";
-
-const Game = () => (
-  <View style={styles.container}>
-    <Text>This is my Game Component!</Text>
-  </View>
-);
+import { emojis } from "../utils";
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +11,43 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 });
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emojis: [],
+      randomQuestion: 0,
+      score: 0
+    };
+    this.restartGame = this.restartGame.bind(this);
+    this.pickRandomQuestion = this.pickRandomQuestion.bind(this);
+  }
+
+  componentDidMount() {
+    this.restartGame();
+  }
+
+  restartGame() {
+    const emojisArr = emojis.slice();
+    const randomQuestion = this.pickRandomQuestion(emojisArr);
+    this.setState({ emojis: emojisArr, randomQuestion, score: 0 });
+  }
+
+  pickRandomQuestion(arr) {
+    return Math.floor(arr.length * Math.random());
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.emojis.map((emoji, index) => (
+          <Text key={index}>{emoji.question}</Text>
+        ))}
+        <Text>SCORE: {this.state.score}</Text>
+      </View>
+    );
+  }
+}
 
 export default Game;
